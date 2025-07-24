fe9c164f139119488a67d9f7c35da83be1c4bfce
“Martin <m.gati@yandex.ru>
2025-07-24 23:56:45 +0000

    Small changes in backend. Added Dockerfiles and docker-compose.

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,14 +1,14 @@
 import ydb
 
 class YDBClient:
-    def __init__(self, endpoint="grpc://localhost:2136", database="/local"):
+    def __init__(self, endpoint="grpc://ydb:2136", database="/local"):
         self.driver_config = ydb.DriverConfig(
             endpoint=endpoint,
             database=database,
             credentials=ydb.AnonymousCredentials(),
         )
         self.driver = ydb.Driver(self.driver_config)
-        self.driver.wait(timeout=5)
+        self.driver.wait(timeout=10)
         self.pool = ydb.QuerySessionPool(self.driver)
 
     def execute(self, query: str, params: dict = None):
